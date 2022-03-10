1f42235c8de0f9107e059d72eabfbbdc7120d483
albmg <albmguer@gmail.com>
2022-03-10 14:31:30 +0000

    added error page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
