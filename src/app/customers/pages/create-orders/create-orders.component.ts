e17e359e94121fe6f0f4b647ba58d85c4e7fb3a0
albmg <albmguer@gmail.com>
2022-03-20 13:21:47 +0000

    added select products and menus on the form

--- src/app/customers/pages/create-orders/create-orders.component.ts
+++ src/app/customers/pages/create-orders/create-orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Menu } from '../../interfaces/menus.interface';
+import { Product } from '../../interfaces/products.interface';
+import { CustomersService } from '../../services/customers.service';
 
 @Component({
   selector: 'app-create-orders',
@@ -8,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrdersComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+
+  menus: Menu[] = []
+
+  constructor( private selectProductService: CustomersService ) { }
 
   ngOnInit(): void {
+
+    this.selectProductService.getProducts()
+    .subscribe( products => this.products = products )
+
+    this.selectProductService.getMenus()
+    .subscribe( menus => this.menus = menus )
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule}  from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -13,10 +16,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
