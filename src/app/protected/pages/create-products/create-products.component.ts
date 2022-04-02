4446a936b1a04693daabb60573cc9510ae780fb9
albmg <albmguer@gmail.com>
2022-04-02 15:17:23 +0100

    added ingredients in create product form

--- src/app/protected/pages/create-products/create-products.component.ts
+++ src/app/protected/pages/create-products/create-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
+import { MatGridTileHeaderCssMatStyler } from '@angular/material/grid-list';
 import { ProtectedService } from '../../services/protected.service';
 
 @Component({
@@ -14,14 +15,35 @@ export class CreateProductsComponent implements OnInit {
   createProductForm: FormGroup = this.fb.group({
     name: ['Papitas super sabrosas', [ Validators.required ]],
     description: ['desde la app', [ Validators.required ]],
-    ingredients: ['["ajo", "agua"]'],
+    ingredients: this.fb.array([], Validators.required ),
     image: ['https://i.blogs.es/cd7416/pexels-photo-1583884/1366_2000.jpg', [ Validators.required ]],
     units: [''],
     weight: ['',],
   })
 
+  newIngredient : FormControl = this.fb.control('', Validators.required)
+
+  get ingredientsArray() {
+    return this.createProductForm.get('ingredients') as FormArray
+  }
+
   constructor( private fb: FormBuilder,
                private saveProductService: ProtectedService ) { }
+
+
+  addIngredient() {
+    if ( this.newIngredient.invalid) {
+      return
+    }
+
+    this.ingredientsArray.push( this.fb.control(this.newIngredient.value, Validators.required) )
+
+    this.newIngredient.reset()
+  }
+
+  removeIngredient( i: number ) {
+    this.ingredientsArray.removeAt(i)
+  }
 
   ngOnInit(): void {}
 
